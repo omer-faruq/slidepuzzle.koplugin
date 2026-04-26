3b9805dc342596ca62cb53cc4fc70766638e8a8a
ömer faruk inel <omer.inel@ciellos.com>
2026-04-26 20:42:42 +0300

    v1.3.0

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "slidepuzzle",
     fullname = _([[Slide Puzzle]]),
     description = _([[Classic NxN sliding tile puzzle (3x3 up to 7x7) with tap and swipe controls, move counter, timer and best-score tracking. Designed to be minimal and e-ink friendly.]]),
-    version = "1.2.0",
+    version = "1.3.0",
 }
